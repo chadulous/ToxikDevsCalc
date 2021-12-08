9f5dd343a797cdd4bff24a532746bff78bbf30d0
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2021-12-08 18:29:35 -0500

    UI Tests

--- build.ps1
+++ build.ps1
@@ -1,9 +1,15 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$message,
+    [Parameter(Mandatory=$true)]
+    [string]$chlog
+)
 Copy-Item -Path ".\public\*" -Destination "./electron" -Recurse -Force
 cd electron 
 npx electron-packager .
-Compress-Archive -Path "./ToxikCalc-win32-x64\*" -Destination "./ToxikCalc-win32-x64.zip"
+Compress-Archive -Path "./ToxikCalc-win32-x64\*" -Destination "./ToxikCalc-win32-x64.zip" -Update
 cd ..
 git add .
-git commit -am %1
+git commit -am $message
 git push
-gh release create %2 -n %1 ./electron/ToxikCalc-win32-x64.zip
+gh release create $chlog -n $message ./electron/ToxikCalc-win32-x64.zip
